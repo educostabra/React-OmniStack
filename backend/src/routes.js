61de14dd200c2e6785dac79c4d0480b9e121b03b
eduardo costa <educostabra@gmail.com>
2019-04-10 22:40:03 -0300

    Concluido conexao com servico MongoDB Atlas com uso da biblioteca mongoose - Aula 2 - 0:28:00

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
+
 const routes = express.Router();
 
 routes.get('/teste', (req, res) => {
-    return res.send('Hello World');
+    return res.send('Hello World...');
 });
 
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://react:react@cluster0-0lsx9.mongodb.net/MyDatabase?retryWrites=true', {
+    useNewUrlParser: true
+})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
